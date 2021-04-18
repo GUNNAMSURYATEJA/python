db0dfca037512b5a453e5e847a6a0ac7765ba49d
Gunnam Surya Surya <>
2021-04-18 13:50:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+if 2 > 5:
+	print("Five is greater than two")
+#no prints
+
+
+x=float(5)
+print(x)
+
+#casting
+psp2k= "pawan kalyan" 
+print(type(psp2k))
+
+hero1, hero2, hero3= "rock ", "mahesh ", "rajni "
+
+print(hero1 + hero2 + hero3)
+print(hero2)
+print(hero3)
+
