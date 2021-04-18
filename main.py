9a0c8231864a281322a0b814c51a2a433c290bdd
SabarIshShankar <>
2021-04-18 14:10:00 +0000

    update 1

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-
-
 if 2 > 5:
 	print("Five is greater than two")
 #no prints
-
-
 x=float(5)
 print(x)
 
@@ -18,3 +14,12 @@ print(hero1 + hero2 + hero3)
 print(hero2)
 print(hero3)
 
+#list 
+bill=['groceries', 'electricity', 'tickets', 'recharge']
+
+p2500, p700,p500, p300 = bill
+
+
+print(p2500)
+print(p500)
+print(p700)
